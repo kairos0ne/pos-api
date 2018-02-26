ab4e4fe26d73e8ce6b339d7b7fbc51d853740500
admin <admin@admins-MacBook-Air.local>
2018-02-26 17:19:04 +0000

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,55 @@
 User.destroy_all
 User.create(name: 'bugs', email: 'bugs@rubyplus.com', password: '123456')
 User.create(name: 'daffy', email: 'daffy@rubyplus.com', password: '123456')
+
+# Start category seeds
+
+Category.destroy_all
+category = Category.create({
+    name: 'Main Meals',
+    description: 'Contains all main meals'
+})
+category.inventory.create(name: "Steak egg and chips", description: "2 eggs steak and chips")
+category.inventory.create(name: "Another meal for testing", description: "Just another meal")
+category.inventory.create(name: "Another meal for testing", description: "Just another meal")
+category.inventory.create(name: "Another meal for testing", description: "Just another meal")
+category.inventory.create(name: "Another meal for testing", description: "Just another meal")
+
+category.save
+
+category = Category.create({
+    name: 'Stater Meals',
+    description: 'Contains all starter meals'
+})
+category.inventory.create(name: "Eggs benadict", description: "Poached egg with holindays sauce")
+category.inventory.create(name: "Another meal for testing", description: "Just another meal")
+category.inventory.create(name: "Another meal for testing", description: "Just another meal")
+category.inventory.create(name: "Another meal for testing", description: "Just another meal")
+category.inventory.create(name: "Another meal for testing", description: "Just another meal")
+
+category.save
+
+category = Category.create({
+    name: 'Kids Meals',
+    description: 'Contains all kids meals'
+})
+category.inventory.create(name: "Hot Dog", description: "Kids hot dog")
+category.inventory.create(name: "Another meal for testing", description: "Just another meal")
+category.inventory.create(name: "Another meal for testing", description: "Just another meal")
+category.inventory.create(name: "Another meal for testing", description: "Just another meal")
+category.inventory.create(name: "Another meal for testing", description: "Just another meal")
+category.inventory.create(name: "Another meal for testing", description: "Just another meal")
+
+category.save
+
+category = Category.create({
+    name: 'Beers',
+    description: 'All beers on sale'
+})
+category.inventory.create(name: "Hogs Back Hog", description: "Fruity ale from Somerset")
+category.inventory.create(name: "Another meal for testing", description: "Just another meal")
+category.inventory.create(name: "Another meal for testing", description: "Just another meal")
+category.inventory.create(name: "Another meal for testing", description: "Just another meal")
+category.inventory.create(name: "Another meal for testing", description: "Just another meal")
+
+category.save
